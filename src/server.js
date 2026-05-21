c4b5753f81c500908edad5e3efacfe9c1e57b031
Vivek Patel <vivekpatel@Viveks-MacBook-Air.local>
2026-05-21 14:37:02 -0400

    Use structured server startup logging

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = parseInt(process.env.PORT || '8080', 10);
 
 const server = stoppable(
   app.listen(port, () => {
-    logger.info(`Server started on port ${port}`);
+    logger.info({ port }, 'Server started');
   })
 );
 
